917e2e5e334d5e35724107aa5033a4d455f45fb0
John Baird <johnbaird@Johns-MacBook-Air.local>
2025-08-22 16:28:16 -0600

    updated gitignore and tf configs for bucket

--- infra/main.tf
+++ infra/main.tf
@@ -1,38 +1,12 @@
-resource "aws_s3_bucket" "bullseye_report" {
-    bucket = var.bucket_name
-    acl    = "public-read"
-
-    policy = jsonencode({
-        Version = "2012-10-17",
-        Statement = [
-        {
-            Sid       = "PublicReadGetObject",
-            Effect    = "Allow",
-            Principal = "*",
-            Action    = "s3:GetObject",
-            Resource  = "${aws_s3_bucket.bullseye_report.arn}/*"
-        }
-        ]
-    })
-
-    tags = {
-        Name        = "bullseye-report.com"
-        Environment = "production"
-    }
-
-    lifecycle {
-        prevent_destroy = true
-    }
-}
-
-resource "aws_s3_bucket_website_configuration" "bullseye_report_website" {
+resource "aws_s3_bucket" "bullseye_forecast" {
   bucket = var.bucket_name
 
-  index_document {
-    suffix = "index.html"
+  tags = {
+    Name        = "bullseye-forecast.com"
+    Environment = "production"
   }
 
-  error_document {
-    key = "index.html"
+  lifecycle {
+    prevent_destroy = true
   }
 }
