dbc588936f70316ad04ea87488be5ca6b7c5a8ea
John Baird <johnbaird@Johns-MacBook-Air.local>
2025-08-22 17:48:38 -0600

    more links

--- frontend/src/pages/About.js
+++ frontend/src/pages/About.js
@@ -29,7 +29,11 @@ function About() {
           interested in collaborating, have questions, or just want to chat, here's where you can reach me:
           <ul>
             <li>Email: jtbaird95@gmail.com</li>
-            <li>GitHub: <a href="https://github.com/jtbaird" target="_blank" rel="noopener noreferrer">github.com/jtbaird</a></li>
+            <li>GitHub:</li>
+            <ul>
+                <li><a href="https://github.com/jtbaird/elk-finder-web" target="_blank" rel="noopener noreferrer">elk-finder-web</a></li>
+                <li><a href="https://github.com/jtbaird/elk-finder" target="_blank" rel="noopener noreferrer">elk-finder</a></li>
+            </ul>
             <li>LinkedIn: <a href="www.linkedin.com/in/john-baird-0b793aa5" target="_blank" rel="noopener noreferrer">linkedin.com/in/john-baird-0b793aa5</a></li>
           </ul>
         </Typography>
